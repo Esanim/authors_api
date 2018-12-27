a8f76917a6806af119de5a4ea6b7409c3b7145f5
Esanim <rhontorn@wp.pl>
2018-12-27 16:07:09 +0100

    Remove elixir build files

--- .elixir_ls/build/test/lib/authors_api/priv/repo/migrations/20181221131231_add_owner_id_to_articles.exs
+++
@@ -1,9 +0,0 @@
-defmodule AuthorsApi.Repo.Migrations.AddOwnerIdToArticles do
-  use Ecto.Migration
-
-  def change do
-    alter table(:articles) do
-      add :owner_id, references(:authors)
-    end
-  end
-end
--- .elixir_ls/build/test/lib/authors_api/priv/repo/migrations/20181222151500_authors_unique_index.exs
+++
@@ -1,7 +0,0 @@
-defmodule AuthorsApi.Repo.Migrations.AuthorsUniqueIndex do
-  use Ecto.Migration
-
-  def change do
-    create unique_index(:authors, [:first_name, :last_name], name: :index_full_name)
-  end
-end
